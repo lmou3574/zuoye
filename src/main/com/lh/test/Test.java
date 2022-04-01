1b4a428491a1509b57a23169bf94632504309e4d
lh <1500957440@qq.com>
2022-04-01 16:52:52 +0800

    刘虎提交

---
+++ src/main/com/lh/test/Test.java
@@ -0,0 +1,11 @@
+package main.com.lh.test;
+
+/**
+ * @author lh
+ * @date 2022/4/1 - 16:51
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
